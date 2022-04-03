eb3a7186bd55d395a0ba106d4422fe17467f2b72
WaveLink <wavelink@email.cz>
2022-04-03 14:33:18 +0200

    Create install.ps1

---
+++ installers/install.ps1
@@ -0,0 +1,7 @@
+mkdir PlaceCZ
+cd PlaceCZ
+Invoke-WebRequest https://raw.githubusercontent.com/PlaceCZ/Bot/master/headlessBot.js -OutFile headlessBot.js
+Invoke-WebRequest https://raw.githubusercontent.com/PlaceCZ/Bot/master/autoLogin.js -OutFile autoLogin.js
+Invoke-WebRequest https://raw.githubusercontent.com/PlaceCZ/Bot/master/package.json -OutFile package.json
+npm install
+echo "Bot je připraven k použití v složce PlaceCZ"
