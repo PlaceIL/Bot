7e2a23b07ede311d6c3f42a7e9264a47c5691ff6
WaveLink <wavelink@email.cz>
2022-04-03 14:32:04 +0200

    Create linux.sh

---
+++ installers/linux.sh
@@ -0,0 +1,7 @@
+mkdir PlaceCZ
+cd PlaceCZ
+wget https://raw.githubusercontent.com/PlaceCZ/Bot/master/headlessBot.js 
+wget https://raw.githubusercontent.com/PlaceCZ/Bot/master/autoLogin.js
+wget https://raw.githubusercontent.com/PlaceCZ/Bot/master/package.json
+npm install
+echo "Bot je připraven k použití v složce PlaceCZ"
